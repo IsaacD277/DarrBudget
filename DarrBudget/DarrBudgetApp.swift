5934a9e44a71164f190b6d76bf28e2f179a4b312
isaacd2 <idarr08@gmail.com>
2024-09-14 01:43:27 -0400

    Initial Commit

---
+++ DarrBudget/DarrBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarrBudgetApp.swift
+//  DarrBudget
+//
+//  Created by Isaac D2 on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DarrBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
